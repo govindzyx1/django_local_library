581b596adeaed008739291033c978f59882a6fcc
Govind Verma <verma.govind92@gmail.com>
2020-01-10 13:29:58 +0530

    First Version

---
+++ catalog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,re_path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),path('books/', views.BookListView.as_view(), name='books'),
+
+    path('book/<int:pk>', views.book_detail_view, name='book-detail'),
+]
+urlpatterns += [   
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
+urlpatterns += [   
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
